964ee8b115ede7ad5771bcd501591134136537d3
Himanshu Khurana <126713985+himanshukhurana2324@users.noreply.github.com>
2024-02-04 21:47:51 +0530

    modified:   db.sqlite3 	modified:   urlproject/__pycache__/views.cpython-311.pyc 	modified:   urlproject/migrations/0001_initial.py 	modified:   urlproject/migrations/__pycache__/0001_initial.cpython-311.pyc 	modified:   urlproject/views.py

--- urlproject/views.py
+++ urlproject/views.py
@@ -46,37 +46,24 @@ def redirect_url(request,shorturl):
     longurl=obj.longurl 
 
 
-    obj.clicks=obj.clicks+1
-    obj.save()
+    
     user_agent = request.META.get('HTTP_USER_AGENT', '').lower()
 
     if 'mobile' in user_agent:
-       obj.mclicks=obj.mclicks+1
+       obj.mclicks=obj.mclicks + 1
+       
+       obj.clicks=obj.clicks + 1
        obj.save()
     else:
        # The request is from a laptop or desktop
        obj.dclicks=obj.dclicks+1
+       
+       obj.clicks=obj.clicks+1
        obj.save()
-
+    
     ip = get_client_ip(request)
     response = DbIpCity.get(ip, api_key='free')
 
-    # country = obj.country
-    # country_count = obj.country_count
-
-    # country_list = country.split(',') if country else []
-    # country_count_list = str(country_count).split(',') if country_count else []
-
-    # if response.country in country_list:
-    #     index = country_list.index(response.country)
-    #     country_count_list[index] = str(int(country_count_list[index]) + 1)
-    # else:
-    #     country_list.append(response.country)
-    #     country_count_list.append(1)
-
-    # obj.country = ','.join(country_list)
-    # obj.country_count = ','.join(str(i) for i in country_count_list)
-    # obj.save()    
     country = obj.country
     country_count = obj.country_count
 
@@ -94,18 +81,6 @@ def redirect_url(request,shorturl):
     obj.country_count = ','.join(country_count_list)
     obj.save()
         
-
-
-    # c_obj = LongToShort.objects.filter(country=response.country)
-    # if c_obj.exists():
-    #     first = c_obj.first()
-    #     first.country_count = first.country_count + 1
-    #     first.save()
-    # else :
-    #     obj.country = response.country
-    #     obj.country_count = obj.country_count + 1
-
-    #     obj.save()
     return redirect(longurl)
 
 
